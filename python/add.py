05ea9ad3d747e83f2c1a7b45e26145210cea5018
DishantDrugkar <dishantdrugkar4@gmail.com>
2025-08-01 13:24:51 +0530

    Addition program with Docker

---
+++ python/add.py
@@ -0,0 +1,8 @@
+print("This is a simple Addition program of two numbers :")
+
+num1=int(input("Enter First Number : "))
+num2=int(input("Enter Second Number : "))
+
+result=num1 + num2
+
+print(f'The sum of two number is {result}.')
